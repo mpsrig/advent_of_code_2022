4a4877a02726dba466b061531fa6243dc5deb858
Michael Stella <michael@michael-stella.info>
2022-12-01 20:25:32 -0800

    day 1 2022

--- src/mpsrig/aoc/year_2022/Day1.java
+++ src/mpsrig/aoc/year_2022/Day1.java
@@ -1,19 +1,23 @@
 package mpsrig.aoc.year_2022;
 
+import mpsrig.aoc.framework.ComputationWithGroups;
 import mpsrig.aoc.framework.Runner;
 
-public class Day1 extends Runner.Computation {
+import java.util.Arrays;
+
+public class Day1 extends ComputationWithGroups {
     public static void main(String[] args) {
         Runner.run(null, new Day1());
     }
 
     @Override
     public Object computePart1() {
-        return null;
+        return groups.stream().mapToInt(x -> x.stream().mapToInt(Integer::parseInt).sum()).max();
     }
 
     @Override
     public Object computePart2() {
-        return null;
+        var sums= groups.stream().mapToInt(x -> x.stream().mapToInt(Integer::parseInt).sum()).sorted().toArray();
+        return sums[sums.length-3] + sums[sums.length-2] + sums[sums.length-1];
     }
 }
