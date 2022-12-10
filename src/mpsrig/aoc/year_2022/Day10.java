4a1ff4de0d50b58967eba02f08cd4d7e16c16a04
Michael Stella <michael@michael-stella.info>
2022-12-09 22:27:46 -0800

    day 10 download

---
+++ src/mpsrig/aoc/year_2022/Day10.java
@@ -0,0 +1,19 @@
+package mpsrig.aoc.year_2022;
+
+import mpsrig.aoc.framework.Runner;
+
+public class Day10 extends Runner.Computation {
+    public static void main(String[] args) {
+        Runner.run(null, new Day10());
+    }
+
+    @Override
+    public Object computePart1() {
+        return null;
+    }
+
+    @Override
+    public Object computePart2() {
+        return null;
+    }
+}
